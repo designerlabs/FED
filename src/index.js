75132682fa974404f334d4d131a620180cdf997a
Sridhar Swaminathan <designerlabs@hotmail.com>
2018-08-26 20:38:37 +0800

    updated

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import './style.css';
+import $ from "jquery";
+import './slider';
+
+
+// When the user clicks the button, open the modal 
+$(".btn_modal").click(function(){
+    $('#myModal').show();
+});
+
+$(".close").click(function(){
+    $('#myModal').hide();
+});
+/* eslint-disable no-console */
+
+
+
---
+++ src/slider.js
@@ -0,0 +1,8 @@
+import $ from "jquery";
+import 'slick-carousel';
+
+
+$(".slider").not('.slick-initialized').slick({
+    dots: true,
+    speed: 500
+})
